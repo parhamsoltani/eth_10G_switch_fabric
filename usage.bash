cbe6fbfd586f981bbe8d83416f99637956f1cf96
parhamsoltani <70743729+parhamsoltani@users.noreply.github.com>
2025-11-28 00:34:16 +0330

    Update usage.bash

--- usage.bash
+++ usage.bash
@@ -11,3 +11,20 @@ run_sim.bat tb_fabric_qos_stress
 
 :: Unit test
 run_sim.bat tb_voq_unit batch
+
+cd sim
+run_sim.bat tb_fabric_qos_sweep
+
+cd sim
+vsim -do "set TB tb_fabric_qos_sweep; do sim_qos.tcl"
+
+cd sim
+vsim -gui
+ModelSim> do sim_qos.tcl
+
+
+cd path\to\your\project
+vivado -mode batch -source vivado_qos_build.tcl
+
+vivado -mode tcl
+Vivado% source vivado_qos_build.tcl
